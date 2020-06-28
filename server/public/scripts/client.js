54f8c49c8d967e7a586d31792d115245177c24a9
Josh Maus <jman985@gmail.com>
2020-06-28 09:54:01 -0500

    intial commit

---
+++ server/public/scripts/client.js
@@ -0,0 +1,11 @@
+console.log('JS is ready');
+
+
+$(document).ready(handleReady);
+
+function handleReady() {
+  console.log("jquery is loaded!")
+
+//   $("#guess-btn").on('click',handleClickGuess)
+//   $("#reset-btn").on('click',handleClickReset)
+}

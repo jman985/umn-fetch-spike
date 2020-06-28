c7207056b2adbf663a607a23738371a86b0af37d
Josh Maus <jman985@gmail.com>
2020-06-28 10:23:54 -0500

    added buttons and inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,43 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!")
 
-//   $("#guess-btn").on('click',handleClickGuess)
-//   $("#reset-btn").on('click',handleClickReset)
+  $("#plus-btn").on('click',handleClickAdd)
+  $("#minus-btn").on('click',handleClickSubtract)
+  $("#times-btn").on('click',handleClickMultiply)
+  $("#divide-btn").on('click',handleClickDivide)
+}
+
+function handleClickAdd(event) {
+    event.preventDefault();
+
+    let num1 = $("#num1").val();
+    let num2 = $("#num2").val();
+  
+    console.log('add clicked');
+}
+function handleClickSubtract(event) {
+    event.preventDefault();
+
+    let num1 = $("#num1").val();
+    let num2 = $("#num2").val();
+  
+    console.log('subtract clicked');
+}
+
+function handleClickMultiply(event) {
+    event.preventDefault();
+
+    let num1 = $("#num1").val();
+    let num2 = $("#num2").val();
+  
+    console.log('multiply clicked');
+}
+
+function handleClickDivide(event) {
+    event.preventDefault();
+
+    let num1 = $("#num1").val();
+    let num2 = $("#num2").val();
+  
+    console.log('divide clicked');
 }

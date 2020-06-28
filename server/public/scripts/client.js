5a2ff9dda8bad1cb4f565a39ccff008290b19d1c
Josh Maus <jman985@gmail.com>
2020-06-28 10:30:59 -0500

    added ajax

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,7 +19,23 @@ function handleClickAdd(event) {
     let num2 = $("#num2").val();
   
     console.log('add clicked');
-}
+
+    $.ajax({
+        type: 'POST',
+        url: '/add',
+        data: { number1: num1  ,
+                number2: num2 }     
+       }).then( function(response) {
+          console.log('Added successfully',response);
+          getHistory();
+        })
+        .catch( function(error) {
+          alert('Sorry, bad things happened. Try again later!');
+          console.log('Error on POST', error)
+        })
+    }
+
+
 function handleClickSubtract(event) {
     event.preventDefault();
 
@@ -27,7 +43,22 @@ function handleClickSubtract(event) {
     let num2 = $("#num2").val();
   
     console.log('subtract clicked');
-}
+
+    $.ajax({
+        type: 'POST',
+        url: '/subtract',
+        data: { number1: num1  ,
+                number2: num2 }     
+       }).then( function(response) {
+          console.log('Subtracted successfully',response);
+          getHistory();
+        })
+        .catch( function(error) {
+          alert('Sorry, bad things happened. Try again later!');
+          console.log('Error on POST', error)
+        })
+    }
+
 
 function handleClickMultiply(event) {
     event.preventDefault();
@@ -36,7 +67,22 @@ function handleClickMultiply(event) {
     let num2 = $("#num2").val();
   
     console.log('multiply clicked');
-}
+
+  $.ajax({
+        type: 'POST',
+        url: '/multiply',
+        data: { number1: num1  ,
+                number2: num2 }     
+       }).then( function(response) {
+          console.log('Multiplied successfully',response);
+          getHistory();
+        })
+        .catch( function(error) {
+          alert('Sorry, bad things happened. Try again later!');
+          console.log('Error on POST', error)
+        })
+    }
+
 
 function handleClickDivide(event) {
     event.preventDefault();
@@ -45,4 +91,23 @@ function handleClickDivide(event) {
     let num2 = $("#num2").val();
   
     console.log('divide clicked');
+
+    $.ajax({
+        type: 'POST',
+        url: '/divide',
+        data: { number1: num1  ,
+                number2: num2 }     
+       }).then( function(response) {
+          console.log('Divided successfully',response);
+          getHistory();
+        })
+        .catch( function(error) {
+          alert('Sorry, bad things happened. Try again later!');
+          console.log('Error on POST', error)
+        })
+    }
+
+
+function getHistory() {
+    
 }
--- server/server.js
+++ server/server.js
@@ -15,3 +15,4 @@ app.use(express.static('server/public'));
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
